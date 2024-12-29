1ae019cc5fa53e65b6641fa7c871a2424cb45393
Clovis Lechien <clovis.lechien@epita.fr>
2024-12-29 15:27:35 +0100

    feat(ero2): reformated some files. will start working on channels and dams.

--- main.py
+++ main.py
@@ -2,43 +2,44 @@ from moulinette import Moulinette, Utilisateur
 from waterfall.waterfall import WaterfallMoulinetteInfinite, WaterfallMoulinetteFinite, WaterfallMoulinetteFiniteBackup
 
 
+def create_user_list(names: list[str]) -> list[Utilisateur]:
+    """
+    Crée une liste d'utilisateurs à partir d'une liste de noms.
+    :param names: Liste de noms.
+    """
+    return [Utilisateur(name) for name in names]
+
+
+def moulinette_loadtest(
+        moulinette: Moulinette | WaterfallMoulinetteInfinite | WaterfallMoulinetteFinite | WaterfallMoulinetteFiniteBackup,
+        user_list: list[Utilisateur],
+        until: int = 20):
+    """
+    Charge test la moulinette avec une liste d'utilisateurs.
+    :param moulinette: type de moulinette.
+    :param user_list: liste d'utilisateurs.
+    :param until: nombre de secondes de la simulation.
+    """
+    for user in user_list:
+        moulinette.add_user(user)
+    moulinette.start_simulation(until=until)
+
+
 if __name__ == "__main__":
+    user_list = create_user_list(["Clovis", "Mael", "Florian", "Alexandre", "Pikachu"])
+
     print("=== Moulinette (Baseline) ===\n")
     moulinette = Moulinette(capacity=2, process_time=2)
-    moulinette.add_user(Utilisateur("Clovis"))
-    moulinette.add_user(Utilisateur("Mael"))
-    moulinette.add_user(Utilisateur("Florian"))
-    moulinette.add_user(Utilisateur("Alexandre"))
-    moulinette.add_user(Utilisateur("Pikachu"))
-    
-    moulinette.start_simulation(until=20)
-    
+    moulinette_loadtest(moulinette, user_list, until=20)
+
     print("\n\n=== Waterfall Moulinette (Infinite Queues) ===\n")
     wm_inf = WaterfallMoulinetteInfinite(test_capacity=2, test_time=2, result_time=2)
-    wm_inf.add_user(Utilisateur("Clovis"))
-    wm_inf.add_user(Utilisateur("Mael"))
-    wm_inf.add_user(Utilisateur("Florian"))
-    wm_inf.add_user(Utilisateur("Alexandre"))
-    wm_inf.add_user(Utilisateur("Pikachu"))
-    
-    wm_inf.start_simulation(until=20)
-    
+    moulinette_loadtest(wm_inf, user_list, until=20)
+
     print("\n\n=== Waterfall Moulinette (Finite Queues) ===\n")
     wm_fin = WaterfallMoulinetteFinite(test_capacity=1, ks=2, kf=2, test_time=1, result_time=1)
-    wm_fin.add_user(Utilisateur("Clovis"))
-    wm_fin.add_user(Utilisateur("Mael"))
-    wm_fin.add_user(Utilisateur("Florian"))
-    wm_fin.add_user(Utilisateur("Alexandre"))
-    wm_fin.add_user(Utilisateur("Pikachu"))
-    
-    wm_fin.start_simulation(until=20)
-    
+    moulinette_loadtest(wm_fin, user_list, until=20)
+
     print("\n\n=== Waterfall Moulinette (Finite Queues with Backup) ===\n")
     wm_fin_back = WaterfallMoulinetteFiniteBackup(test_capacity=1, ks=2, kf=2, test_time=1, result_time=1)
-    wm_fin_back.add_user(Utilisateur("Clovis"))
-    wm_fin_back.add_user(Utilisateur("Mael"))
-    wm_fin_back.add_user(Utilisateur("Florian"))
-    wm_fin_back.add_user(Utilisateur("Alexandre"))
-    wm_fin_back.add_user(Utilisateur("Pikachu"))
-    
-    wm_fin_back.start_simulation(until=20)
+    moulinette_loadtest(wm_fin_back, user_list, until=20)
--- moulinette.py
+++ moulinette.py
@@ -10,10 +10,10 @@ class Utilisateur:
     :param promo: Promotion de l'étudiant
     :param workshop: Atelier auquel l'étudiant participe
     """
-    def __init__(self, name="Anonymous", student_id=None, promo="ING1", workshop="C"):
+    def __init__(self, name: str = "Anonymous", student_id: int = None, promo: str = "ING1", workshop: str = "C"):
         self.name = name
         self.student_id = student_id
-        if student_id == None:
+        if student_id is None:
             self.student_id = random.randint(1000, 10000)
         self.promo = promo
         self.workshop = workshop
@@ -33,7 +33,7 @@ class Utilisateur:
             "workshop": self.workshop
         }
     
-    def __setitem__(self, name, student_id, promo, workshop):
+    def __setitem__(self, name: str, student_id: int, promo: str, workshop: str):
         self.name = name
         self.student_id = student_id
         self.promo = promo
@@ -46,7 +46,7 @@ class Moulinette:
     :param capacity: Nombre d'utilisateur en simultanée dans la file.
     :param process_time: Temps de process d'un utilisateur dans la file.
     """
-    def __init__(self, capacity=1, process_time=1):
+    def __init__(self, capacity: int = 1, process_time: int = 1):
         self.env = simpy.Environment()
         self.server = simpy.Resource(self.env, capacity=capacity)
         self.process_time = process_time
@@ -56,8 +56,8 @@ class Moulinette:
     Ajoute un nouvel utilisateur dans la moulinette.
     :param user: Utilisateur.
     """
-    def add_user(self, user=None):
-        if user == None:
+    def add_user(self, user: Utilisateur = None):
+        if user is None:
             user = Utilisateur()
         self.users.append(user)
 
@@ -65,7 +65,7 @@ class Moulinette:
     Simule l'exécution d'un test pour un utilisateur.
     :param user: Utilisateur.
     """
-    def run_test(self, user):
+    def run_test(self, user: Utilisateur):
         print(f"{user} enters the queue at {self.env.now}")
         with self.server.request() as request:
             yield request
@@ -77,7 +77,7 @@ class Moulinette:
     Lance une simulation complète sur tous les utilisateurs dans la moulinette.
     :param until: Limite de temps de la simulation.
     """
-    def start_simulation(self, until=20):
+    def start_simulation(self, until: int = 20):
         for user in self.users:
             self.env.process(self.run_test(user))
         self.env.run(until=until)
